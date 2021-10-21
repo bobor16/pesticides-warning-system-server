e4312b93a6244c498f1b21abfc8f416e48da067f
bobor16 <bobor16@student.sdu.dk>
2021-10-21 17:18:48 +0200

    added config file and gitignore

--- src/dataFrame.py
+++ src/dataFrame.py
@@ -1,29 +1,31 @@
 import folium
 import geopandas as gpd
 from sentinelsat.sentinel import SentinelAPI
-import rasterio 
+import rasterio
 import matplotlib.pyplot as plt
 from rasterio import plot
 from rasterio.plot import show
 from rasterio.mask import mask
 from osgeo import gdal
+import config as cfg
 
 m = folium.Map([latitude, longitude], zoom_start=11)
-boundary = gpd.read_file(r'C:\Users\thoma\Desktop\map.geojson')
+boundary = gpd.read_file(r'map.geojson')
 folium.GeoJson(boundary).add_to(m)
+m
 
 footprint = None
 for i in boundary['geometry']:
     footprint = i
-    
-user = bobor 
-password = 12345678
+
+user = cfg.user
+password = cfg.password
 api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
 products = api.query(footprint,
-                     date = ('20200109', '20200510'),
-                     platformname = 'Sentinel-2',
-                     processinglevel = 'Level-2A',
-                     cloudcoverpercentage = (0, 20))
+                     date=('20200109', '20200510'),
+                     platformname='Sentinel-2',
+                     processinglevel='Level-2A',
+                     cloudcoverpercentage=(0, 20))
 
 gdf = api.to_geodataframe(products)
 gdf_sorted = gdf.sort_values(['cloudcoverpercentage'], ascending=[True])
